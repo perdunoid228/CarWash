ac6926f878d0784d7d5b9ff5ff7371d87352998f
perdunoid228 <andreyandrey12321@mail.ru>
2025-10-30 23:03:12 +0300

    1

--- src/CarWash.Application/obj/Debug/net9.0/CarWash.Application.AssemblyInfo.cs
+++ src/CarWash.Application/obj/Debug/net9.0/CarWash.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarWash.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+939c70553891f6c345a6ca3f37214eccaba26f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarWash.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarWash.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/CarWash.Domain/obj/Debug/net9.0/CarWash.Domain.AssemblyInfo.cs
+++ src/CarWash.Domain/obj/Debug/net9.0/CarWash.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarWash.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+939c70553891f6c345a6ca3f37214eccaba26f5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarWash.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarWash.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
